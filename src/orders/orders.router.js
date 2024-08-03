0e815ef5a9e59b6891fe4a2953c439e618e20d6c
sdiachenko <sdiachenko@indeed.com>
2024-08-03 11:32:21 -0500

    take2

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -4,7 +4,7 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 const notFound = require("../errors/notFound");
 
 // TODO: Implement the /orders routes needed to make the tests pass
-router.route("/:orderId").get(controller.read).put(controller.update).delete(controller.delete).all(notFound);
-router.route("/").get(controller.list).post(controller.create).all(notFound);
+router.route("/:orderId").get(controller.read).put(controller.update).delete(controller.delete).all(methodNotAllowed);
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
 
 module.exports = router;
